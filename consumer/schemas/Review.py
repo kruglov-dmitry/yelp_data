f3921c241d66473aa2d6b10395eac90a014d18eb
dkruglov <kruglov.dima@gmail.com>
2020-06-20 20:36:54 +0200

    Add Review schema

---
+++ consumer/schemas/Review.py
@@ -0,0 +1,15 @@
+from pyspark.sql import types as T
+
+BUSINESS_SCHEMA = T.StructType(
+    [
+        T.StructField("review_id", T.StringType(), True),
+        T.StructField("user_id", T.StringType(), True),
+        T.StructField("business_id", T.StringType(), True),
+        T.StructField("stars", T.IntegerType(), True),
+        T.StructField("date", T.DateType(), True),
+        T.StructField("text", T.StringType(), True),
+        T.StructField("useful", T.IntegerType(), True),
+        T.StructField("funny", T.IntegerType(), True),
+        T.StructField("cool", T.IntegerType(), True),
+    ]
+)
