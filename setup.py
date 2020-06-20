5468df879b62aa20f2e6e75d3a06d091448c310a
dkruglov <kruglov.dima@gmail.com>
2020-06-20 17:09:40 +0200

    add project layout

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages
+from setuptools import setup
+
+NAME = "yelp_consumer"
+
+VERSION = "1.0.0"
+
+REQUIRES = [
+
+]
+
+DEV_REQUIRES = {
+    'dev': [
+        'tox'
+    ]
+}
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Simple module to stream data from kafka into cassandra",
+    author_email="kruglov.dima@gmail.com",
+    keywords=["cassandra", "kafka"],
+    install_requires=REQUIRES,
+    extras_require=DEV_REQUIRES,
+    dependency_links=[],
+    packages=find_packages(),
+    package_data={
+        'yelp_consumer': ['*.sh',
+                          '*.md']
+    }
+)
