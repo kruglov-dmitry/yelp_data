b77d20348a3c3d3b10327eab3ef52a84d4b225ca
dkruglov <kruglov.dima@gmail.com>
2020-06-21 17:44:36 +0200

    Update queries

--- create_cassandra_schema.sh
+++ create_cassandra_schema.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 CASSANDRA_CONTAINER_NAME=cassandra1
-SCHEMA_FOLDER=./schemas/
 SCHEMA_FOLDER_IN_DOCKER=/schemas/
 
-DOCKER_EXEC="sudo docker exec -it ${CASSANDRA_CONTAINER_NAME} cqlsh ${CASSANDRA_CONTAINER_NAME} -f "
+sudo docker exec -it ${CASSANDRA_CONTAINER_NAME} ${SCHEMA_FOLDER_IN_DOCKER}/deploy_schema.sh
 
-for filename in ${SCHEMA_FOLDER}*.cql; do
-    schema_file=${filename##*/}
-    echo ${DOCKER_EXEC} ${SCHEMA_FOLDER_IN_DOCKER}${schema_file}
-done
