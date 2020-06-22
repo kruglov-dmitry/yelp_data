7c1253210d591d16d5495d594cf5d8bf6ff56e22
dkruglov <kruglov.dima@gmail.com>
2020-06-23 01:17:47 +0200

    add checkin transform

--- consumer/etls/checkin.py
+++
@@ -1,2 +0,0 @@
-def checkin_transform():
-    pass
--- consumer/etls/transforms.py
+++ consumer/etls/transforms.py
@@ -1,15 +1,16 @@
 from consumer.constants import BUSINESS_TOPIC, REVIEW_TOPIC, USER_TOPIC, CHECKIN_TOPIC, TIP_TOPIC
-from consumer.etls.business import business_transform
-from consumer.etls.review import review_transform
-from consumer.etls.user import user_transform
-from consumer.etls.checkin import checkin_transform
-from consumer.etls.tip import tip_transform
+from consumer.transform.business import business_transform
+from consumer.transform.review import review_transform
+from consumer.transform.user import user_transform
+from consumer.transform.checkin import checkin_transform
+from consumer.transform.tip import tip_transform
 
 
 W = {
     BUSINESS_TOPIC: business_transform,
+    CHECKIN_TOPIC: checkin_transform,
+
     REVIEW_TOPIC: review_transform,
     USER_TOPIC: user_transform,
-    CHECKIN_TOPIC: checkin_transform,
     TIP_TOPIC: tip_transform
 }
